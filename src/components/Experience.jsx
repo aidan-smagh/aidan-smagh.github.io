0fa8a9033e3decdee0e235fa4fa378933a85f839
aidan-smagh <aidansmagh@gmail.com>
2026-05-05 13:38:59 -0400

    Start grid pattern for the experience and add Alterea info

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,14 @@
 import eServices from '/eServices.jpg'
+import alterea from '/alterea.jpg'
 
-
-const ExperienceEntry = ({ logo, companyTitle, jobTitle, descriptionBullets }) => (
-    <div className="flex justify-between px-56 py-8">
-        <img src={logo} className="w-64" />
-        <div>
+const ExperienceEntry = ({ logo, companyTitle, jobTitle, date, descriptionBullets, logoClassName="w-72" }) => (
+    <div className="grid grid-cols-2 gap-4 px-64">
+        <img src={logo} className={logoClassName} />
+        <div className="w-80">
             <h2>{companyTitle}</h2>
             <h3>{jobTitle}</h3>
-         <ul>
+            <p>{date}</p>
+            <ul>
                 {descriptionBullets.map(bullet => <li>{bullet}</li>)}
             </ul>
         </div>
@@ -18,10 +19,20 @@ function Experience() {
     return (
         <div data-aos="fade-up" data-aos-duration="2000" className="pb-32">
             <h1>Experience</h1>
-            <ExperienceEntry logo={eServices}
+            <ExperienceEntry 
+                logo={eServices}
                 companyTitle="eServices Payment Technology"
                 jobTitle="Software Engineer-Summer Intern"
+                date="May 2026-August 2026"
                 descriptionBullets={["TBD"]}>
+            </ExperienceEntry>
+            <ExperienceEntry
+                logo={alterea}
+                companyTitle="Alterea Inc"
+                jobTitle="Game Development Intern"
+                date="Jan 2026-May 2026"
+                descriptionBullets={["something"]}
+                logoClassName="w-32">
             </ExperienceEntry>
         </div>
 
