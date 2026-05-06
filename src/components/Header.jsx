d1edaf21a22698135b570a7a6e038f233e2a9a94
aidan-smagh <aidansmagh@gmail.com>
2026-05-06 12:55:28 -0400

    Add name to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,11 +15,14 @@ const NavLink = ({ href, children }) => (
 
 function Header() {
     return (
-        <div className="flex justify-end px-4 py-4 gap-4 fixed bg-slate-100 w-screen">
+        <div className="flex justify-between px-4 py-4 fixed bg-slate-100 w-screen">
+            <p className="text-left font-bold">Aidan Smagh</p>
+            <div className="flex gap-4">
             <NavLink href="https://github.com/aidan-smagh">Github</NavLink>
             <NavLink href="https://www.linkedin.com/in/aidan-smagh-b189242a7/">LinkedIn</NavLink>
             <NavLink href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=aidansmagh@gmail.com">Email</NavLink>
             <NavLink href="Aidan_Smagh_Resume.pdf">Resumé</NavLink>
+            </div>
         </div>
     );
 }
