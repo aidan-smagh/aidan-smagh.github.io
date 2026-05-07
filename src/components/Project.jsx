021c2b1ce1ebf7746ce3d5e82399cc473f761e9a
aidan-smagh <aidansmagh@gmail.com>
2026-05-06 20:57:13 -0400

    update AOI image and tweak project grid

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,11 +1,11 @@
-import aoi from '/aoi.jpeg'
+import aoi from '/aoi.jpg'
 
 const ProjectEntry = ({ title, techStack, link, image, extra}) => (
     <div className="w-64 h-84 bg-black rounded-md hover:scale-103 transition-transform duration-500">
-        <img src={image} className="w-64 h-64 rounded-md"></img>
+        <img src={image} className="w-64 h-64 rounded-md mx-auto block"></img>
         <h2 className="!text-white">{title}</h2>
         <ul className="flex flex-wrap justify-center mt-2 gap-1">
-            {techStack.map(pill => <li className="inline-flex text-xs bg-blue-300 rounded-sm px-2">{pill}</li>)}
+            {techStack.map(pill => <li className="inline-flex text-xs text-white bg-blue-500 rounded-sm px-2">{pill}</li>)}
         </ul>
         <p className="text-white text-xs">{extra}</p>
     </div>
@@ -13,9 +13,25 @@ const ProjectEntry = ({ title, techStack, link, image, extra}) => (
 
 function Project() {
     return(
-        <div data-aos="fade-up" data-aos-duration="2000" className="pb-32">
+        <div data-aos="fade-up" data-aos-duration="2000">
             <h1>Projects</h1>
-            <div className="grid grid-cols-2 gap-4 px-64 py-8">
+            <div className="grid grid-cols-3 py-8 px-48 justify-items-center">
+                <ProjectEntry
+                    title="Agents of Influence"
+                    image={aoi}
+                    description="some words to fill out the website so i can come back and fix it later"
+                    techStack={["Unity", "C#", "Firebase"]}
+                    extra="* in collaboration with Alterea Inc"
+                    >
+                </ProjectEntry>
+                <ProjectEntry
+                    title="Agents of Influence"
+                    image={aoi}
+                    description="some words to fill out the website so i can come back and fix it later"
+                    techStack={["Unity", "C#", "Firebase"]}
+                    extra="* in collaboration with Alterea Inc"
+                    >
+                </ProjectEntry>
                 <ProjectEntry
                     title="Agents of Influence"
                     image={aoi}
